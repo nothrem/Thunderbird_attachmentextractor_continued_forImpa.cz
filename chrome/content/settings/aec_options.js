b4cbe780c746396150dc97a698e6d51a5527c48a
ThunderbirdMailDE <github@thunderbird-mail.de>
2019-07-27 10:12:38 +0200

    Replace license headers with short MPL 2.0 headers

--- chrome/content/settings/aec_options.js
+++ chrome/content/settings/aec_options.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // global AEC preferences
 var AECprefs = Cc["@mozilla.org/preferences-service;1"]
   .getService(Ci.nsIPrefBranch);
