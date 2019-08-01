8e37f3a6855f0dc8bc2e5aeeefa6e253934cd7fc
ThunderbirdMailDE <github@thunderbird-mail.de>
2019-08-01 19:35:56 +0200

    Remove obsolet console.log lines

--- chrome/content/aec_dialog_filenamePattern.js
+++ chrome/content/aec_dialog_filenamePattern.js
@@ -1,5 +1,4 @@
 function add_to_pattern(button) {
-  console.log("add_to_pattern");
   let fnpbox=document.getElementById('filenamepattern');
   let postindex=fnpbox.selectionStart+button.label.length; 
   fnpbox.value=fnpbox.value.substring(0,fnpbox.selectionStart)+button.label+fnpbox.value.substring(fnpbox.selectionEnd);
@@ -7,13 +6,11 @@ function add_to_pattern(button) {
 }
 
 function onload() {
-  console.log("onload");
   document.getElementById('filenamepattern').value = window.arguments[0].value; 
   document.getElementById('savecheck').checked = window.arguments[1].value;
 }
 
 function onaccept() { 
-  console.log("onaccept");
   window.arguments[0].value=document.getElementById('filenamepattern').value; 
   window.arguments[1].value=document.getElementById('savecheck').checked; 
   window.arguments[2].value=true;
